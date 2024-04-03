2c024581763bd073dcc59f06f0abf27a322129df
Zoltan Nagy <abesto0@gmail.com>
2024-04-03 14:54:58 +0100

    Drop `test.ts`

--- servers/home/test.ts
+++
@@ -1,7 +0,0 @@
-import { echoClient } from "services/echo/client";
-
-export const main = async (ns: NS) => {
-  ns.clearLog();
-  const echo = echoClient(ns);
-  ns.print(await echo.echo("Hello, World!"));
-};
